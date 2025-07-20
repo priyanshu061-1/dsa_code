fe1e6a26a459d4c25a1983f449205a988b1015a6
Priyanshu dubey <134142739+priyanshu061-1@users.noreply.github.com>
2025-07-20 21:19:09 +0530

    Add files via upload

---
+++ poweroftwo.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+#include<math.h>
+int main(){
+int n=16;
+int i;
+for(i=0;i<=30;i++){
+    int ans=pow(2,1);
+
+    if(ans==n)
+    {
+        return true;
+
+    }
+}
+return false;
+
+}
+
